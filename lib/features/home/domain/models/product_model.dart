8bd5ff41ab8d60e1c95937142354112e951fb971
Omar Khaled Ammar <omark4367@gmail.com>
2025-05-16 01:57:41 +0300

    - Created app_colors.dart and product_model.dart

---
+++ lib/features/home/domain/models/product_model.dart
@@ -0,0 +1,77 @@
+class ProductModel {
+  int? id;
+  String? title;
+  String? description;
+  String? category;
+  num? price;
+  num? discountPercentage;
+  num? rating;
+  num? stock;
+  num? weight;
+  String? warrantyInformation;
+  String? shippingInformation;
+  String? availabilityStatus;
+  String? returnPolicy;
+  num? minimumOrderQuantity;
+  List<String>? images;
+  String? thumbnail;
+
+  ProductModel({
+    this.id,
+    this.title,
+    this.description,
+    this.category,
+    this.price,
+    this.discountPercentage,
+    this.rating,
+    this.stock,
+    this.weight,
+    this.warrantyInformation,
+    this.shippingInformation,
+    this.availabilityStatus,
+    this.returnPolicy,
+    this.minimumOrderQuantity,
+    this.images,
+    this.thumbnail,
+  });
+
+  factory ProductModel.fromJson(Map<String, dynamic> json) => ProductModel(
+        id: json['id'],
+        title: json['title'],
+        description: json['description'],
+        category: json['category'],
+        price: json['price'],
+        discountPercentage: json['discountPercentage'],
+        rating: json['rating'],
+        stock: json['stock'],
+        weight: json['weight'],
+        warrantyInformation: json['warrantyInformation'],
+        shippingInformation: json['shippingInformation'],
+        availabilityStatus: json['availabilityStatus'],
+        returnPolicy: json['returnPolicy'],
+        minimumOrderQuantity: json['minimumOrderQuantity'],
+        images: (json['images'] as List<dynamic>?)
+            ?.map((e) => e as String)
+            .toList(),
+        thumbnail: json['thumbnail'],
+      );
+
+  Map<String, dynamic> toJson() => {
+        'id': id,
+        'title': title,
+        'description': description,
+        'category': category,
+        'price': price,
+        'discountPercentage': discountPercentage,
+        'rating': rating,
+        'stock': stock,
+        'weight': weight,
+        'warrantyInformation': warrantyInformation,
+        'shippingInformation': shippingInformation,
+        'availabilityStatus': availabilityStatus,
+        'returnPolicy': returnPolicy,
+        'minimumOrderQuantity': minimumOrderQuantity,
+        'images': images,
+        'thumbnail': thumbnail,
+      };
+}
---
+++ lib/utils/app_colors.dart
@@ -0,0 +1,7 @@
+import 'dart:ui';
+
+class AppColors {
+  static const Color primaryColor = Color (0xFF102F6B);
+  static const Color secondaryColor = Color (0xFFCA6118);
+}
+
